d04c856c18459cd3c87d0ec8dc54f1a35d821abe
zakura0 <simon.kazemi99@gmail.com>
2025-01-07 20:22:43 +0100

    init and structs

---
+++ go.mod
@@ -0,0 +1,3 @@
+module OttoAssignment
+
+go 1.23.1
---
+++ src/models/models.go
@@ -0,0 +1,18 @@
+package models
+
+// Post repräsentiert einen Beitrag eines Nutzers
+type Post struct {
+	UserID int    `json:"userId"`
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+	Body   string `json:"body"`
+}
+
+// Comment repräsentiert einen Kommentar zu einem Beitrag
+type Comment struct {
+	PostID int    `json:"postId"`
+	ID     int    `json:"id"`
+	Name   string `json:"name"`
+	Email  string `json:"email"`
+	Body   string `json:"body"`
+}
